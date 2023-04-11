eeffd9e5eeb660d721ea8b420d75ef27a73a3b5f
Tayler Edelen <TaylerLEdelen@gmail.com>
2023-04-10 19:36:33 -0500

    Finished chapter 6 studio

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,15 +1,83 @@
 package test;
-
+import main.BalancedBrackets;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
+
 
     //TODO: add tests here
     @Test
     public void emptyTest() {
         assertEquals(true, true);
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void doubleBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+
+    @Test
+    public void noBracketReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void tripleBracketReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[]]]"));
+    }
+
+    @Test
+    public void prettyStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+    @Test
+    public void bunkStringReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void uglyStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+
+    @Test
+    public void singleBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void reversedBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+//    @Test
+//    public void expectedOutcomeOfOneBracketReturnsFalse() {
+//        BalancedBrackets.setMyField(0);
+//        assertEquals(0, 1);
+//    }
+//
+//    @Test
+//    public void expectedOutcomeOfTwoBracketsReturnsTrue() {
+//        assertEquals(BalancedBrackets.hasBalancedBrackets(0, 2));
+//    }
+
+    @Test public void enclosedStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test public void partiallyEnclosedStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test public void bracketsBeforeStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
     }
 
 
